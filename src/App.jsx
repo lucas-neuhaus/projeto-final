a1ac3a06e9d50b6b21ccaf8537d36edd719f96e3
odley13 <franciscofrancieldo83@gmail.com>
2023-07-20 02:07:52 -0300

    Rotas criadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
+import Login from "./pages/Login";
+import Cadastrar from "./pages/Cadastrar";
+import Estoque from "./pages/Estoque";
+import Armazem from "./pages/Armazem";
 
-  return (
-    <>Hello World</>
-  )
-}
+import Home from "./components/Home"
 
-export default App
+
+
+const App = () => {
+  return ( 
+    
+    <>
+    <Home />
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/Login" element={<Login />}></Route>
+        <Route path="/Cadastrar" element={<Cadastrar />}></Route>
+        <Route path="/Estoque" element={<Estoque />}></Route>
+        <Route path="/Armazem" element={<Armazem />}></Route>
+
+      </Routes>
+    </BrowserRouter>
+    </>
+   
+  );
+};
+
+export default App;
---
+++ src/pages/Estoque/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Estoque() {
+  return (
+    <div>Estoque</div>
+  )
+}
