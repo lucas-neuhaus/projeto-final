8c80275dae2010fc77c024d3ddc8a8d7c92dfb83
Matheus_S-Cruz <matheus_silva-cruz@estudante.sesisenai.org.br>
2023-07-26 23:19:30 -0300

    Adiciona cadastro de produtos ao estoque

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Cadastrar from "./pages/Cadastrar";
 import EstoqueLista from "./pages/EstoqueLista";
+import EstoqueCadastro from "./pages/EstoqueCadastro";
 import Armazem from "./pages/Armazem";
 
 import Home from "./pages/Home";
@@ -16,6 +17,7 @@ const App = () => {
             <Route path="login" element={<Login />}></Route>
             <Route path="cadastrar" element={<Cadastrar />}></Route>
             <Route path="estoque" element={<EstoqueLista />}></Route>
+            <Route path="estoqueCadastro" element={<EstoqueCadastro />}></Route>
             <Route path="armazem" element={<Armazem />}></Route>
           </Route>
         </Routes>
