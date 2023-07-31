5ac7b6cc0f4eee1710c972e68de65560e84ffc47
Lucas Neuhaus <lucasneuhaus@hotmail.com>
2023-07-30 23:13:22 -0300

    corrige erro merge

--- src/components/input/index.jsx
+++ src/components/input/index.jsx
@@ -1,16 +1,40 @@
 import "./input.css";
 
 const Input = (props) => {
-  return (
-    <div className="inputConteiner">
-      <label className="inputEtiqueta" htmlFor={props.id}>
-        {props.etiqueta}
-      </label>
-      <div className="inputId">
-        <input id={props.id} type={props.tipo} onChange={props.aoMudar} />
+  if (props.tipo === "select") {
+    return (
+      <div className="inputConteiner">
+        <label className="inputEtiqueta" htmlFor={props.id}>
+          {props.etiqueta}
+        </label>
+        <div className="inputId">
+          <select
+            id={props.id}
+            value={props.valor}
+            onChange={props.aoMudar}
+          >
+            {props.children}
+          </select>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="inputConteiner">
+        <label className="inputEtiqueta" htmlFor={props.id}>
+          {props.etiqueta}
+        </label>
+        <div className="inputId">
+          <input
+            id={props.id}
+            type={props.tipo}
+            onChange={props.aoMudar}
+            value={props.valor}
+          />
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Input;
